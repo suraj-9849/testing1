a6356dafbab5ede9318ed294150ca12e350c19e4
GUNUPURU SURAJ <146669517+suraj-9849@users.noreply.github.com>
2024-09-17 16:16:37 +0530

    sjdsakdj

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ const vision = require('@google-cloud/vision');
 const app = express();
 const port = process.env.PORT || 3000; 
 
-app.use(cors({ origin: '*' }));
+const allowedOrigins = ['http://localhost:5173'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1 || origin === '*') {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json({ limit: '10mb' }));
 
